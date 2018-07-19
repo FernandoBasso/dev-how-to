fbc59939d45dc47d41ce4ca3093beaabac73b489
Fernando Basso <fernandobasso.br@gmail.com>
2018-07-19 07:19:42 -0300

    updates.

--- ruby/well-grounded-rubyist/05-self-default-obj/10b-cvars.rb
+++ ruby/well-grounded-rubyist/05-self-default-obj/10b-cvars.rb
@@ -1,20 +1,40 @@
+#
+# A class with a cvar and a civar (but not an ivar).
+#
+# cvars are visible from:
+#   - the toplevel of the class
+#   - inside class methods
+#   - inside instance methods
+#
+# In short:
+#
+# Class methods can access:
+#   - class variables
+#   - class instance variables
+#
+# Instance methods can access:
+#   - class variables
+#   - instance variables
+#
+
 class Foo
   @@z = 'a cvar'
   @z = 'a civar'
 
   # A class method (which is a method on the object Foo in this
-  # example). It can access both cvars and civars.
+  # example). Inside a class method, both cvars and civars can
+  # be accessed.
   def self.print_vars
-    p @@z # works from class method.
-    p @z # works from class method.
+    p @@z
+    p @z
   end
 
   # An instance method. It can access cvars and ivars, but not civars. Note
   # that inside this instance method @z is not the same as the previous @z
   # defined at the toplevel of the class.
   def print_vars
-    p @@z # works from instance method.
-    p @z #
+    p @@z
+    p @z # <1>
   end
 end
 
@@ -22,8 +42,11 @@ Foo.print_vars
 Foo.new.print_vars
 
 #
-# cvars are visible from:
-#   - the toplevel of the class
-#   - inside class methods
-#   - inside instance methods
+# <1>. Here, inside an instance method, `@z' is an instance variable. As in
+# this example class we _don't_ have an ivar (we have a cvar and a civar), when
+# we try to `p @z', which is an ivar at this location, we get `nil'.
 #
+
+
+
+
