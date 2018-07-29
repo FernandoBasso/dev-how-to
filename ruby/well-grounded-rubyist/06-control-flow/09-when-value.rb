997530e924383c37b96136a83f49e914ec566b61
Fernando Basso <fernandobasso.br@gmail.com>
2018-07-29 11:44:02 -0300

    Updates...

--- ruby/well-grounded-rubyist/06-control-flow/09-when-value.rb
+++ ruby/well-grounded-rubyist/06-control-flow/09-when-value.rb
@@ -4,18 +4,20 @@ x, y = 5, 6
 
 puts case
   when x > y
-    return x + y
-    exit
+    x + y
   when x < y
-    y - x
-    exit
+    x - y
   when x == y
     x
   else
     0
   end
 
+# → -1
+
 #
-# If nothing matches, not even the else (which is not the case for this
-# example), then `nil' is returned.
+# If nothing matches, not even the `else' (which is not the case for this
+# example), then `nil' is returned. In this example, `-1' is returned, and
+# therefore printed by `puts'.
 #
+
