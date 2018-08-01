1aa5f7df7083a5a2b8fb96ea652d49d958092d9c
Fernando Basso <fernandobasso.br@gmail.com>
2018-08-01 07:04:11 -0300

    Updates...

--- ruby/well-grounded-rubyist/06-control-flow/17-blocks-precedence.rb
+++ ruby/well-grounded-rubyist/06-control-flow/17-blocks-precedence.rb
@@ -4,11 +4,13 @@
 
 arr = 1, 3, 5
 
+# `puts' gets `arr.map' as argument, and the block is bound to `arr.map'.
 puts arr.map { |e| e * 2} # <1>
 # →  2
 # →  6
 # →  10
 
+# `puts' receives both `arr.map' and the block as arguments.
 puts arr.map do |e| e * 2 end # <2>
 # → #<Enumerator:0x000055a9f3394138>
 
@@ -27,4 +29,11 @@ puts arr.map do |e| e * 2 end # <2>
 # the block. So the do/end version is really equivalent to
 #
 #   puts array.map
+#
+# If you supply a code block but the method you call doesn’t see it (or doesn’t
+# look for it), no error occurs: methods aren’t obliged to yield, and many
+# methods (including map ) have well-defined behaviors for cases where there’s
+# a code block and cases where there isn’t. If a method seems to be ignoring a
+# block that you expect it to yield to, look closely at the precedence rules
+# and make sure the block really is available to the method.
 #
