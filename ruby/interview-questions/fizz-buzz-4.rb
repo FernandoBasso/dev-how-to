f5f5aa4805c509dfdd81a43035724243eabbfb15
Fernando Basso <fernandobasso.br@gmail.com>
2019-02-08 06:26:41 -0200

    fizzbuzz parameterized

--- ruby/interview-questions/fizz-buzz-4.rb
+++ ruby/interview-questions/fizz-buzz-4.rb
@@ -14,3 +14,12 @@ end
 
 fizzbuzz(1..25, [['Fizz', 3], ['Buzz', 5]])
 
+#
+# We've introduced a new concept: triggers. A trigger is the pairing of a
+# divisor and an output string. There is no official name for this pairing, due
+# to FizzBuzz being a synthetic problem as opposed to a real-world problem, but
+# it's not that uncommon. We create abstract models of data and processes, and
+# these models contain things that need to be named. Often times there is a
+# pre-existing name we can use, but sometimes not.
+#
+
