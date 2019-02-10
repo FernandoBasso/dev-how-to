d246c3f346907cb2825f3133ad082948ca5a3be4
Fernando Basso <fernandobasso.br@gmail.com>
2019-02-10 11:56:20 -0200

    fizzbuzz with a generator

--- ruby/interview-questions/fizz-buzz-6.rb
+++ ruby/interview-questions/fizz-buzz-6.rb
@@ -2,9 +2,9 @@
 # https://www.tomdalling.com/blog/software-design/fizzbuzz-in-too-much-detail/
 #
 
-def fizzbuzz(range, trigger)
+def fizzbuzz(range, triggers)
   range.map do |num|
-    parts = trigger.select { |(_, predicate)| predicate.call(num) }
+    parts = triggers.select { |_, predicate| predicate.call(num) } # <1>
     # puts parts
     parts.size > 0 ? parts.map(&:first).join : num
   end
@@ -15,4 +15,3 @@ puts fizzbuzz(1..25, [
   ['Buzz', -> (n){ n % 5 == 0 }],
   ['Zazz', -> (n){ n < 10 }],
 ])
-
