420e3c77facce0bd3b1530cd100d1956e2363cfe
Fernando Basso <fernandobasso.br@gmail.com>
2019-02-20 06:54:31 -0300

    wip: ruby stuff

--- ruby/interview-questions/dev1.rb
+++ ruby/interview-questions/dev1.rb
@@ -1,21 +1,25 @@
-myvar = 'global'
+# Private, protected, public methods
 
-module Foo
-  myvar = 'local'
+class Enemy
+  def name=(name)
+    p 'name='
+    p self
+    @name = name
+  end
 
-  class Bar
-    def thing1
-      myvar
+  def yell_name
+    p "yell_name: #{self}"
+    yell
+  end
+
+  private
+
+    def yell
+      p "yell: #{self}"
+      @name.upcase
     end
-  end
 end
 
-class Foo::Bar
-  def thing2
-    myvar
-  end
-end
-
-p Foo::Bar.new.thing1
-p Foo::Bar.new.thing2
-
+enemy1 = Enemy.new
+enemy1.name = 'Vader'
+p enemy1.yell_name
