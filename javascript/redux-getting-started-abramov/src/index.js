49c83d2449700e5cd81ed63d7fc77f037f63f2b5
Fernando Basso <fernandobasso.br@gmail.com>
2019-12-26 15:15:43 -0300

    more redux notes and examples

--- javascript/redux-getting-started-abramov/src/index.js
+++ javascript/redux-getting-started-abramov/src/index.js
@@ -10,4 +10,9 @@ import './index.css';
 // import './v06-store-methods';
 // import './v07-impl-store-from-scratch';
 // import './v08-react-counter';
-import './v09-no-array-mutation';
+// import './v09-no-array-mutation';
+// import './v10-no-object-mutation';
+// import './v11-add-todo';
+import './v12-toggle-todo';
+
+
