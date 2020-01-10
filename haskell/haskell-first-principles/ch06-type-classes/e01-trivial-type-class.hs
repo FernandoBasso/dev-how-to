2a00603c77cb1ed18dca1d4ba83e64975c8cf603
Fernando Basso <fernandobasso.br@gmail.com>
2020-01-10 16:02:26 -0300

    haskell chapter 6 initial notes

---
+++ haskell/haskell-first-principles/ch06-type-classes/e01-trivial-type-class.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+data Trivial =
+  Trivial'
+
+--
+-- Implements Eq type class for the Trivial data type.
+--
+instance Eq Trivial where
+  Trivial' == Trivial' = True
+
+--
+-- Used ' at the end of the data constructor to avoid confusion. Trivial'
+-- is the data constructor and Trivial is the type constructor.
+--
+-- Trivial' is data, like 9, or "foo". You compare the data, not the type.
+--
+-- λ> Trivial' == Trivial'
+-- True
+--
