37e6932946bcd8a37cef518445059feaf8cb2057
Fernando Basso <fernandobasso.br@gmail.com>
2022-04-03 07:26:45 -0300

    typescript(ts50): Add UserPrefs with type

---
+++ typescript/ts50/ch05-generics/l33a-videos.ts
@@ -0,0 +1,39 @@
+export const NAME = "l33a Mapped Type Modifiers";
+
+const log: Console["log"] = console.log.bind(console);
+
+type VideoFormatURLs = {
+  format360p: URL;
+  format480p: URL;
+  format720p: URL;
+  format1080p: URL;
+};
+
+type SubtitleURLs = {
+  german: URL;
+  french: URL;
+  english: URL;
+};
+
+declare const videos: VideoFormatURLs;
+
+//
+// ‘Split’ would probably be more well named if we choose
+// something like ‘ToUnion’ perhaps.
+//
+type Split<Obj> = {
+  [Prop in keyof Obj]: Record<Prop, Obj[Prop]>;
+}[keyof Obj];
+
+type AvailableFormats = Split<VideoFormatURLs>;
+
+const s1: AvailableFormats = { format480p: new URL("/videos") };
+
+type UserPrefs = {
+  format: keyof VideoFormatURLs;
+  subtitles: {
+    active: boolean;
+    language: keyof SubtitleURLs;
+  };
+  theme: "light" | "dark";
+};
