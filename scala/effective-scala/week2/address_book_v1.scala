90926101ca7e7b3e6bb028ff0fed67193346204d
Fernando Basso <fernandobasso.br@gmail.com>
2024-05-31 18:26:39 -0300

    scala: Address book example

--- scala/effective-scala/week2/address_book_v1.scala
+++ scala/effective-scala/week2/address_book_v1.scala
@@ -1,7 +1,7 @@
 package address_book_v1
 
 //
-// Lists can store zero or more of particular type of item.
+// Lists can store zero or more of a particular type of item.
 // Order is maintained.
 //
 
@@ -12,3 +12,12 @@ case class Contact(
 )
 
 case class AddressBook(contacts: List[Contact])
+
+val alice = Contact("Alice", "alice@example.dev", List())
+val bob = Contact("Bob", "bob@example.dev", List("+5512341234"))
+val addrBook = AddressBook(List(alice, bob))
+
+@main def main: Unit =
+  println(addrBook.contacts.map(_.name))
+  println(addrBook.contacts.map(_.email))
+  println(addrBook.contacts.map(c => List(c.name, c.email)))
