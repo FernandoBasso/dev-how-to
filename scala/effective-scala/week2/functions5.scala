2c00748ac3990ddac04db14f7772472a17043e64
Fernando Basso <fernandobasso.br@gmail.com>
2024-06-01 15:06:02 -0300

    scala: More function exmaples

--- scala/effective-scala/week2/functions5.scala
+++ scala/effective-scala/week2/functions5.scala
@@ -18,6 +18,33 @@ val bob = Contact("Bob", "bot@example.dev")
 val endsWithScaDotLa: Contact => Boolean =
   _.email.endsWith("@sca.la")
 
+//
+// Placeholder arguments can be explicitly type-annotated too if needed.
+//
+
+val endsWithDotDev: Contact => Boolean =
+  (_: Contact).email.endsWith(".dev")
+
+//
+// And multiple placeholders works too!
+//
+
+val add: (Int, Int) => Int = _ + _
+
+//
+// And sometimes _ is the wildcard argument. Note _ is not used. The
+// function always returns 42.
+//
+val f: Int => Int = _ => 42
+
+//
+// Compare placeholder vs wildcard!
+//
+val placeholder = (_: Int) + 1
+val wildcard = (_: Int) => 42
+
 @main def main(): Unit =
   println(endsWithScaDotLa(alice))
   println(endsWithScaDotLa(bob))
+  println(endsWithDotDev(bob))
+  println(add(-1, 1))
