de87c83d8f2b0f3d87be634dbaec5f21e6741920
Fernando Basso <fernandobasso.br@gmail.com>
2024-06-08 11:22:04 -0300

    fpts: Update notes and examples on compose

---
+++ fpts/notes/src/compose_v3.ts
@@ -0,0 +1,45 @@
+const log: Console["log"] = console.log.bind(console);
+
+function toStr(v: number): string {
+  return v.toString();
+}
+
+function inc(x: number): number {
+  return x + 1;
+}
+
+/**
+ * Applies the functions right-to left.
+ */
+function compose(
+  g: (x: number) => string,
+  f: (x: number) => number,
+): (x: number) => string {
+  return function composed(x: number): string {
+    return g(f(x));
+  };
+}
+
+/**
+ * Increments `x` then returns it as a string.
+ */
+const incThenStr: (x: number) => string = compose(toStr, inc);
+
+const res1: string = incThenStr(0);
+log(res1, typeof res1);
+//=> 1    string
+
+function double(x: number): number {
+  return x * 2;
+}
+
+/**
+ * Doubles `x` then returns it as a string.
+ */
+const doubleThenStr: (x: number) => string = compose(toStr, double);
+
+const res2: string = doubleThenStr(7);
+log(res2, typeof res2);
+//=> 14   string
+
+
