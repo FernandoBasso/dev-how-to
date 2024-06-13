b9fc1130119602b1c93b86762168eeb20021be55
Fernando Basso <fernandobasso.br@gmail.com>
2024-06-13 17:21:28 -0300

    fpts: Add intro none and some()

---
+++ fpts/notes/src/e18.ts
@@ -0,0 +1,29 @@
+import { curry2, compose2 } from "./lib";
+
+const log: Console["log"] = console.log.bind(console);
+
+const none = Symbol('None');
+type None = typeof none;
+
+function some<T>(val: T): { val: T } {
+  return { val };
+}
+
+type Some<V> = { val: V };
+
+type Option<T> = Some<T> | None;
+
+function inc(x: number): number {
+  return x + 1;
+}
+
+function div(dividend: number, divisor: number): Option<number> {
+  if (divisor === 0) return none;
+  return some(dividend / divisor);
+}
+
+log(div(8, 2));
+//=> { val: 4 }
+
+log(div(8, 0));
+//=> Symbol(None)
