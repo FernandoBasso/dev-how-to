8a027e2f876ec157a47c494798b7eba747f8c989
Fernando Basso <fernandobasso.br@gmail.com>
2024-07-09 09:06:21 -0300

    hackerrank(algds): Rename x to p

--- hackerrank/algds/easy/number-line-jumps/js/kangaroo_jumps_v2.js
+++ hackerrank/algds/easy/number-line-jumps/js/kangaroo_jumps_v2.js
@@ -8,17 +8,17 @@
  * - T.C: O(1).
  * - S.C: O(1).
  *
- * @param {number} x1 The position of kangaroo 1.
- * @param {number} v1 The jump veolcity of kangaroo 1.
- * @param {number} x2 The position of kangaroo 2.
- * @param {number} v2 The jump veolcity of kangaroo 2.
+ * @param {number} p1 The position of kangaroo 1.
+ * @param {number} v1 The jump velocity of kangaroo 1.
+ * @param {number} p2 The position of kangaroo 2.
+ * @param {number} v2 The jump velocity of kangaroo 2.
  * @returns {"YES" | "NO"}
  */
-function kangaroo(x1, v1, x2, v2) {
+function kangaroo(p1, v1, p2, v2) {
   if (v1 <= v2)
     return "NO";
 
-  if ((x2 - x1) % (v1 - v2) === 0)
+  if ((p2 - p1) % (v1 - v2) === 0)
     return "YES";
 
   return "NO";
