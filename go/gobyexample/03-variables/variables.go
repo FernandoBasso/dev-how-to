74545e078211dab2894c80786c62a9a2adea9a31
Fernando Basso <fernandobasso.br@gmail.com>
2024-07-14 09:51:01 -0300

    go: Notes on variables

---
+++ go/gobyexample/03-variables/variables.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+  var a = "initial"
+  fmt.Println(a)
+
+  var x, y int = 1, 2
+  fmt.Println(x, y)
+
+  var z int
+  var n int = 3
+  fmt.Println(z, n)
+
+  var s string
+  fmt.Println("«" + s + "»")
+
+  f := "Leia"
+  var g string = "Organa"
+  fmt.Println(f, g)
+}
