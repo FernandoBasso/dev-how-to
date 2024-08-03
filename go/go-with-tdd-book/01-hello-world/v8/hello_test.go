8141dd21a509293424a5e0a3abd3391e6ecf4e18
Fernando Basso <fernandobasso.br@gmail.com>
2024-08-03 09:02:49 -0300

    go(tdd-book): Add Hello v8 with switch

---
+++ go/go-with-tdd-book/01-hello-world/v8/hello_test.go
@@ -0,0 +1,41 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Lauren", "French")
+		want := "Bonjour, Lauren!"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
