2acfd0142305b5722a94ac0d690d2750b9f528f0
Fernando Basso <fernandobasso.br@gmail.com>
2024-08-05 18:56:23 -0300

    go(tdd-book): Add anonymous struct and for loop unit test

---
+++ go/go-with-tdd-book/05-structs/v05/shapes.go
@@ -0,0 +1,35 @@
+package main
+
+import "math"
+
+// Shape is implemented by anything that can tell us its area.
+type Shape interface {
+	Area() float64
+}
+
+// Rectangle has the dimensions of a rectangle
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+// Circle has the radio of a circle.
+type Circle struct {
+	Radius float64
+}
+
+// Area returns the area of a rectangle.
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+// Perimeter returns the perimeter of a rectangle.
+func Perimeter(rect Rectangle) float64 {
+	return 2 * (rect.Width + rect.Height)
+}
+
+// Area returns the area of a circle.
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
---
+++ go/go-with-tdd-book/05-structs/v05/shapes_test.go
@@ -0,0 +1,31 @@
+package main
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rect := Rectangle{10.0, 10.0}
+	got := Perimeter(rect)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f\nwant %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want float64
+	}{
+		{Rectangle{12, 5}, 72},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+
+		if got != tt.want {
+		  t.Errorf("\ngot %g\nwant %g", got, tt.want)
+		}
+	}
+}
