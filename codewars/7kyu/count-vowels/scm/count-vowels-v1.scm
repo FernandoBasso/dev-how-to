9efeac60ac90377cb60ad5d1d8e6a42da5ab7154
Fernando Basso <fernandobasso.br@gmail.com>
2024-08-23 10:36:16 -0300

    codewars(7kyu): Count Vowels in scheme

---
+++ codewars/7kyu/count-vowels/scm/count-vowels-v1.scm
@@ -0,0 +1,25 @@
+;;
+;; tags: codewars algorithm vowel count
+;;
+
+(import (only srfi-13 string-fold string-index string-contains-ci))
+
+;;;;
+;; Count the number of vowels in s.
+;;
+;(define (count-vowels str)
+;  (string-fold (lambda (c acc)
+;                 (if (or (string-index "aeiou" c)
+;                         (string-index "AEIOU" c))
+;                     (+ acc 1)
+;                     acc))
+;               0
+;               str))
+
+(define (count-vowels s)
+  (string-fold (lambda (c acc)
+                 (if (string-contains-ci "aeiou" (string c))
+                     (+ acc 1)
+                     acc))
+               0
+               s))
