60a8c4d370d183189e0d3bf373370e33489bd0dd
Fernando Basso <fernandobasso.br@gmail.com>
2024-10-25 17:28:07 -0300

    fp: Add example with number comparison

---
+++ lambdafun/03.rb
@@ -0,0 +1,37 @@
+Identity = ->x { x }
+
+True = ->first{->second{first}}
+False = ->first{->second{second}}
+
+If = ->bool{->trueCase{->falseCase{
+  bool[trueCase][falseCase].(Identity)
+}}}
+
+Assert = ->bool {
+  If[bool][->_{ puts "✔" }][->_{ puts "✘" }]
+}
+
+Refute = ->bool {
+  If[bool][->_{ puts "✘" }][->_{ puts "✔" }]
+}
+
+$zero = ->_{True}
+
+Succ = ->n{ ->_{False} }
+IsZero = ->n{ n.(Identity) }
+
+NumbersEqual = ->first{->second{
+  If[IsZero[first]][
+    ->_{IsZero[second]}
+  ][
+    ->_{False}
+  ]
+}}
+
+Assert[IsZero[$zero]]
+Assert[NumbersEqual[$zero][$zero]]
+Assert[NumbersEqual[Succ[$zero]][Succ[$zero]]]
+
+Refute[IsZero[Succ[$zero]]]
+Refute[NumbersEqual[Succ[$zero]][$zero]]
+Refute[NumbersEqual[$zero][Succ[$zero]]]
