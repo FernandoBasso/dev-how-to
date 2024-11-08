22080d3275a30dd6c76178f349c635bf1345d021
Fernando Basso <fernandobasso.br@gmail.com>
2024-11-08 16:57:45 -0300

    go: Shapes v1

---
+++ go/go-with-tdd-book/shapes/shapes.go
@@ -0,0 +1,13 @@
+package shapes
+
+// Perimeter calculates the perimeter of a rectangle given
+// its width and height.
+func Perimeter(width, height float64) float64 {
+	return 2 * (width + height)
+}
+
+// Area calculates the area of a rectangle given
+// its width and height
+func Area(width, height float64) float64 {
+	return width * height
+}
---
+++ go/go-with-tdd-book/shapes/shapes_test.go
@@ -0,0 +1,26 @@
+package shapes_test
+
+import (
+	"gowithtdd/shapes"
+	"testing"
+)
+
+func TestPerimeter(t *testing.T) {
+	t.Run("can calculate the perimeter", func(t *testing.T) {
+		got := shapes.Perimeter(10.0, 10.0)
+		want := 40.0
+
+		if got != want {
+			t.Errorf("got %.2f, want %.2f", got, want)
+		}
+	})
+
+	t.Run("can calculate the area", func(t *testing.T) {
+		got := shapes.Area(2.0, 3.0)
+		want := 6.0
+
+		if got != want {
+			t.Errorf("got %.2f, want %.2f", got, want)
+		}
+	})
+}
