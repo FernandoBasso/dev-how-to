ee9033844ad7898d026de45046557b33aceecd11
Fernando Basso <fernandobasso.br@gmail.com>
2025-02-13 10:25:43 -0300

    go(generics): Explain why scalev1 does not compile

--- src/go/generics/scalev1/scale_v1.go
+++ src/go/generics/scalev1/scale_v1.go
@@ -35,6 +35,37 @@ func main() {
 
 	scaledXs := scale(xs, 2)
 
+	// ERROR: Doesn't compile.
 	fmt.Printf("%s\n", scaledXs.Str())
 	// ~ scaledXs.Str undefined (type []int32 has no field or method Str)
 }
+
+/*
+
+The problem is that scale returns a []E, where E is the element type of
+the argument slice.
+
+When we call scale() with a value of type Point, whose underlying type
+is []int32, we get back a value of type []int32 (not a value of type
+Point).
+
+Point has the method Str, but []int32 does not, thus the error.
+
+NOTE: This is one more reason why I generally prefer explicit type
+annotations. So instead of:
+
+    v := someFn(x)
+
+We would do something like this:
+
+		var v SomeType = someFn(x)
+
+If somFn() does NOT return SomeType, we know immediately.
+
+Explicit type annotations make the expected type immediately visible,
+and we don't need to be in an editor/IDE with LSP or some other tool to
+help with hovers or whatever to inspect the returned types. It becomes
+visible and explicit even in a plain text file, or in a source control
+vendor like Gitlab.
+
+*/
